88f61001d1cd63377f4258b741edc997444664ae
liyongfang <786419142@qq.com>
2015-10-08 16:08:23 +0800

    新建项目

---
+++ MyWeiBoo/MyWeiBoo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyWeiBoo
+//
+//  Created by 李永方 on 15/10/8.
+//  Copyright © 2015年 李永方. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
